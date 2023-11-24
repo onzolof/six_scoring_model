39cbfa9dc4e2709aa8044ca20810dec5dc5ad0ef
onzolof <onzolof@mailfence.com>
2023-11-24 11:27:55 +0100

    calculate score correctly

--- app.py
+++ app.py
@@ -1,4 +1,88 @@
-import numpy as np
+import collections
 
-def calculate_score(coefficients: np.array, values: np.array):
-    return
+import streamlit as st
+
+from model import calculate_normalized_regression_score
+
+st.set_page_config(
+    page_title="Score Calculator",
+    page_icon="➕",
+)
+
+variables = {
+    "effort": {
+        "event_type_complexity": {
+            "label": "Event Type Complexity",
+            "coefficient": "0.2",
+            "value": "0.2"
+        },
+        "security_type_complexity": {
+            "label": "Security Type Complexity",
+            "coefficient": "0.05",
+            "value": "0.15"
+        }
+    },
+    "criticality": {
+
+    }
+}
+default_values = dict(
+    coeff=dict(
+        event_type_complexity=0.2,
+        security_type_complexity=0.1,
+    )
+)
+
+
+def calculate_effort_score():
+    coefficients, values = get_params("effort")
+    st.session_state["effort_score"] = calculate_normalized_regression_score(coefficients, values)
+
+
+def get_params(prefix):
+    coefficients = [st.session_state[prefix + "_constant"]]
+    values = []
+    for param_key in variables[prefix].keys():
+        coefficients = [*coefficients, st.session_state[prefix + "_coeff_" + param_key]]
+        values = [*values, st.session_state[prefix + "_value_" + param_key]]
+    return coefficients, values
+
+
+tab_effort, tab_criticality, tab_prio = st.tabs(["Effort", "Criticality", "Combined"])
+
+with tab_effort:
+    st.header("Effort Prediction", divider="gray")
+    st.text("Predict the effort of an SWIFT message.")
+
+    effort_config = variables["effort"]
+
+    with st.expander("Coefficients"):
+        st.text(
+            "Define the weight for each feature.This values can either be defined\nqualitatively or learned quantitatively on historical data.",)
+
+        st.session_state["effort_constant"] = st.number_input(label="Constant", disabled=True, value=0.0)
+
+        for key, config in effort_config.items():
+            st.session_state["effort_coeff_" + key] = st.number_input(label=config["label"], min_value=0., max_value=1.,
+                                                                      value=float(config["coefficient"]), step=0.05,
+                                                                      on_change=calculate_effort_score)
+
+    with st.expander("Calculation"):
+        st.latex("Define the weight for each feature.")
+
+    for key, config in effort_config.items():
+        st.session_state["effort_value_" + key] = st.slider(label=config["label"], min_value=0., max_value=1.,
+                                                            value=float(config["value"]), step=0.05,
+                                                            on_change=calculate_effort_score)
+
+    calculate_effort_score()
+
+    st.success("Effort Score: " + str(st.session_state["effort_score"]))
+
+with tab_criticality:
+    st.header("Criticality Prediction")
+    st.text("Predict the criticality of an event.")
+
+with tab_prio:
+    st.header("Calculating Prioritization Score")
+    st.text("Combine the effort and the criticality predictions into a single score.")
--- model.py
+++ model.py
@@ -1,13 +1,33 @@
 import numpy as np
 
-def calculate_regression_score(coefficients: list, values: list):
+
+def _calculate_regression_score(coefficients: list, values: list):
+    constant = coefficients[0]
+    weights_array = np.array(coefficients[1:])
+    values_array = np.array(values)
+    assert len(weights_array) == len(values_array)
+    score = constant + np.sum(weights_array * values_array)
+    return score
+
+
+def _normalize(score, coefficients: list, values: list):
+    """
+    round(((value - min) / (max-min) * 9) + 1)
+    """
+    min_score = _calculate_regression_score(coefficients, [0] * len(values))
+    max_score = _calculate_regression_score(coefficients, [1] * len(values))
+    normalized_score = (score - min_score) / (max_score - min_score)
+    number_of_classes = 9
+    scaled_score = int(round((normalized_score * number_of_classes) + 1))
+    return scaled_score
+
+
+def calculate_normalized_regression_score(coefficients: list, values: list):
     """
     coefficients: beta_0, beta_1, ..., beta_n
     values: x_1, x_2, ..., x_n
     returns: y_predicted
     """
-    constant = coefficients[0]
-    weights = np.array(coefficients[1:])
-    values = np.array(values)
-    assert len(weights) == len(values)
-    return constant + np.sum(weights * values)
+    score = _calculate_regression_score(coefficients, values)
+    normalized_score = _normalize(score, coefficients, values)
+    return normalized_score
