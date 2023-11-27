c7420a0cac1e152e78ffcf4122c828deb7546322
onzolof <onzolof@mailfence.com>
2023-11-27 11:34:14 +0100

    implement latex for both scores

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-from model import calculate_normalized_regression_score
+from model import calculate_normalized_regression_score, build_regression_formula_in_latex
 
 st.set_page_config(
     page_title="Score Calculator",
     page_icon="➕",
+    layout="wide"
 )
 
 variables = {
@@ -38,11 +39,22 @@ variables = {
 def calculate_complexity_score():
     coefficients, values = get_params("complexity")
     st.session_state["complexity_score"] = calculate_normalized_regression_score(coefficients, values)
+    param_names = snake_to_pascal_case(variables["complexity"].keys())
+    st.session_state["complexity_formula"] = build_regression_formula_in_latex(param_names, coefficients, values)
 
 
 def calculate_criticality_score():
     coefficients, values = get_params("criticality")
     st.session_state["criticality_score"] = calculate_normalized_regression_score(coefficients, values)
+    param_names = snake_to_pascal_case(variables["complexity"].keys())
+    st.session_state["criticality_formula"] = build_regression_formula_in_latex(param_names, coefficients, values)
+
+
+def snake_to_pascal_case(keys) -> list:
+    param_names = []
+    for key_name in keys:
+        param_names = [*param_names, key_name.replace("_", " ").title().replace(" ", "")]
+    return param_names
 
 
 def get_params(prefix):
@@ -63,7 +75,8 @@ with tab_complexity:
     complexity_config = variables["complexity"]
 
     with st.expander("Coefficients"):
-        st.text( "Define the weight for each feature. This values can either be defined\nqualitatively (expert model) or learned quantitatively on historical\ndata (statistical model).")
+        st.text(
+            "Define the weight for each feature. This values can either be defined\nqualitatively (expert model) or learned quantitatively on historical\ndata (statistical model).")
 
         st.session_state["complexity_constant"] = st.number_input(label="Constant Complexity", disabled=True, value=0.0)
 
@@ -73,15 +86,16 @@ with tab_complexity:
                                                                           value=float(config["coefficient"]), step=0.05,
                                                                           on_change=calculate_complexity_score)
 
+    calculate_complexity_score()
+
     with st.expander("Calculation"):
-        st.latex("Define the weight for each feature.")
+        if "complexity_formula" in st.session_state:
+            st.latex(st.session_state["complexity_formula"])
 
     for key, config in complexity_config.items():
         st.session_state["complexity_value_" + key] = st.slider(label=config["label"], min_value=0., max_value=1.,
                                                                 value=float(config["value"]), step=0.05,
                                                                 on_change=calculate_complexity_score)
-
-    calculate_complexity_score()
 
     st.success("Complexity Score: " + str(st.session_state["complexity_score"]))
 
@@ -95,7 +109,8 @@ with tab_criticality:
         st.text(
             "Define the weight for each feature. This values can either be defined\nqualitatively (expert model) or learned quantitatively on historical\ndata (statistical model).", )
 
-        st.session_state["criticality_constant"] = st.number_input(label="Constant Criticality", disabled=True, value=0.0)
+        st.session_state["criticality_constant"] = st.number_input(label="Constant Criticality", disabled=True,
+                                                                   value=0.0)
 
         for key, config in criticality_config.items():
             st.session_state["criticality_coeff_" + key] = st.number_input(label=config["label"], min_value=0.,
@@ -104,15 +119,16 @@ with tab_criticality:
                                                                            step=0.05,
                                                                            on_change=calculate_criticality_score)
 
+    calculate_criticality_score()
+
     with st.expander("Calculation"):
-        st.latex("Define the weight for each feature.")
+        if "criticality_formula" in st.session_state:
+            st.latex(st.session_state["criticality_formula"])
 
     for key, config in criticality_config.items():
         st.session_state["criticality_value_" + key] = st.slider(label=config["label"], min_value=0., max_value=1.,
                                                                  value=float(config["value"]), step=0.05,
                                                                  on_change=calculate_criticality_score)
-
-    calculate_criticality_score()
 
     st.success("Criticality Score: " + str(st.session_state["criticality_score"]))
 
--- model.py
+++ model.py
@@ -14,15 +14,23 @@ def _normalize(score, coefficients: list, values: list):
     """
     round(((value - min) / (max-min) * 9) + 1)
     """
-    min_score = _calculate_regression_score(coefficients, [0] * len(values))
-    max_score = _calculate_regression_score(coefficients, [1] * len(values))
+    min_score = _calc_min_score(coefficients, values)
+    max_score = _calc_max_score(coefficients, values)
     normalized_score = (score - min_score) / (max_score - min_score)
     number_of_classes = 9
     scaled_score = int(round((normalized_score * number_of_classes) + 1))
     return scaled_score
 
 
-def calculate_normalized_regression_score(coefficients: list, values: list):
+def _calc_max_score(coefficients, values):
+    return _calculate_regression_score(coefficients, [1] * len(values))
+
+
+def _calc_min_score(coefficients, values):
+    return _calculate_regression_score(coefficients, [0] * len(values))
+
+
+def calculate_normalized_regression_score(coefficients: list, values: list) -> int:
     """
     coefficients: beta_0, beta_1, ..., beta_n
     values: x_1, x_2, ..., x_n
@@ -31,3 +39,26 @@ def calculate_normalized_regression_score(coefficients: list, values: list):
     score = _calculate_regression_score(coefficients, values)
     normalized_score = _normalize(score, coefficients, values)
     return normalized_score
+
+
+def build_regression_formula_in_latex(param_names: list, coefficients: list, values: list) -> str:
+    string = r"f_\text{scoring}(\cdot) = \beta_0"
+    for i, param_name in enumerate(param_names):
+        number = str(i + 1)
+        string = string + r"+ \beta_{\text{" + number + r"}} * x_{\text{" + param_name + r"}}"
+    string += r"\\=" + str(coefficients[0])
+    for i, param_name in enumerate(param_names):
+        string = string + r"+ " + str(coefficients[i + 1]) + " * " + str(values[i])
+    score = str(round(_calculate_regression_score(coefficients, values), 2))
+    string += r"\\=" + score
+
+    string += r"\\f_\text{norming}(\cdot) = round((\frac{f_\text{scoring} - min}{max - min} * 9) + 1)"
+
+    min_score = str(_calc_min_score(coefficients, values))
+    max_score = str(_calc_max_score(coefficients, values))
+    string += r"\\= round((\frac{" + score + "-" + min_score + "}{" + max_score + "-" + min_score + "} * 9) + 1)"
+
+    normalized_score = str(calculate_normalized_regression_score(coefficients, values))
+    string += r"\\= " + normalized_score
+
+    return string
